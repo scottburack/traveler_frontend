499fe35e8160e08f405bfad0900b49f5b643cef7
scott <you@example.comitsmescottb123@gmail.com>
2018-03-26 16:41:57 -0400

    added columns to trips

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+const BASE_URL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?"
+
 document.addEventListener("DOMContentLoaded", function(){
+  fetch(BASE_URL + 'location=long?beach', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${API_KEY}`
+    }
+  })
+  .then(resp => resp.json())
+  .then(json => console.log(json))
 
   
+
+
 
 })
